bd280d3f36a46816ecd8c4e69197fcdd37ec5cab
Cron3x <craipy.exe@gmail.com>
2022-08-08 18:52:34 +0200

    now circle in the background

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ function autoFocus(){
 	inp = document.getElementById("prompt");
 	$( document ).on( 'keydown', function ( e ) {
 		inp.focus();
-	  });
+	});
+	mouse_hover();
 }
 
 function parse() {
@@ -39,4 +40,18 @@ function parse() {
 	document.getElementById("return").innerHTML = text;
 	document.getElementById("return").className = color;
 	window = window.open(url, '_self');
+}
+
+function mouse_hover() {
+	var cursor = document.getElementById("cursor");
+	document.body.addEventListener("mousemove", function(e) {
+		// cursor.style.setProperty('--x', e.clientX+"px");
+		// cursor.style.setProperty('--y', e.clientY+"px");
+
+		document.body.style.backgroundColor = `#1c1c1c`;
+		document.body.style.backgroundImage = `radial-gradient(circle at ${e.clientX}px ${e.clientY}px, #45523b 0%, rgba(28, 28, 28, 1) calc(6%))`;
+
+		//cursor.style.setProperty('--bg', bg);
+		console.log(document.body.style.backgroundImage);
+	});
 }
